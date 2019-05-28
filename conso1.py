e933ec312d45ecddc40bd70ecc0f9ef7f3a4c368
pavithraabhi <44343365+pavithraabhi@users.noreply.github.com>
2019-05-28 19:41:28 +0530

    Create conso1.py

---
+++ conso1.py
@@ -0,0 +1,9 @@
+a=['a','e','i','o','u']
+b=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+c=input()
+if c in a:
+    print("Vowel")
+elif c in b:
+    print("Consonant")
+else:
+    print("invalid")
